d1f71d37cfb2c6e6a2f26668054768b0d34383c5
jthlesi <distanc87@gmail.com>
2022-06-04 02:18:02 +0900

    0604_8

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,8 +13,8 @@ var arrImg = ['../img/banner_1.jpg','../img/banner_2.jpg','../img/banner_3.jpg',
 var i = 1
 
 setInterval( function(){
-        $("#banner").css("background-image","url("+arrImg[i]+")");
+        $("#banner").css('background-image','url(../img/banner_'+i+'.jpg)');
         i++;
         if (i>=arrImg.length){
-            i=0;
+            i=1;
         }},3000)
