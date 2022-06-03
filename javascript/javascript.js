b53d73a2dead44793a569341fb076554fe59f645
jthlesi <distanc87@gmail.com>
2022-06-04 02:04:17 +0900

    0604_7

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,9 +12,9 @@ window.onresize = function () {
 var arrImg = ['../img/banner_1.jpg','../img/banner_2.jpg','../img/banner_3.jpg','../img/banner_4.jpg']
 var i = 1
 
-// setInterval( function(){
-//         document.getElementById("banner").style.backgroundImage=`url(${arrImg[i]})`;
-//         i++;
-//         if (i>=arrImg.length){
-//             i=0;
-//         }},3000)
+setInterval( function(){
+        $("#banner").css("background-image","url("+arrImg[i]+")");
+        i++;
+        if (i>=arrImg.length){
+            i=0;
+        }},3000)
