ce8bbea7d5567c70129b4aec44673a4624b4ef57
jthlesi <distanc87@gmail.com>
2022-06-07 02:27:53 +0900

    0606

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,6 @@
 //배너 높이
-$(document).ready(function(){
-    $("#banner").css("height",$(window).height()+'px');
+$(document).ready(function () {
+    $("#banner").css("height", $(window).height() + 'px');
 });
 
 //리사이즈
@@ -9,11 +9,27 @@ window.onresize = function () {
 };
 
 //배너
-var i = 2
+var idx = 2
+setInterval(function () {
+    $("#banner").css('background-image', 'url(img/banner_' + idx + '.jpg)');
+    idx++;
+    if (idx > 4) {
+        idx = 1;
+    }
+}, 4000)
 
-setInterval( function(){
-        $("#banner").css('background-image','url(img/banner_'+i+'.jpg)');
-        i++;
-        if (i>4){
-            i=1;
-        }},4000)
+//작은슬라이드
+var wrapWidth = document.querySelector('#slides').offsetWidth;
+var slidesBox = document.querySelector("#slide_list");
+var slideBox = document.querySelectorAll('.slide_box');
+var imgBox = document.querySelectorAll('.img_box');
+
+for(i=0;i<imgBox.length;i++){
+imgBox[i].style.backgroundImage = 'url(img/small_'+(i+1)+'.jpg)';
+};
+
+  for (i = 0; i < slideBox.length; i++) {
+    slideBox[i].style.width = wrapWidth / 4 + "px";
+  };
+
+
