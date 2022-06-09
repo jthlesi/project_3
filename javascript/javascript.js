a747684f4ee0547e310e0348b9572bd255f6fefb
jthlesi <distanc87@gmail.com>
2022-06-09 18:34:53 +0900

    0609

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -63,3 +63,65 @@ $("#prev").click(function(){
 
 setInterval(smallSlide, 3000)
 
+//section#3 크기
+$(document).ready(function () {
+    $("#photo").css("flex-basis", ($(window).width() / 2) + 'px');
+    $("#photo").css("left", -(($(window).width() / 2) - (($(".container").width() / 2))) + 'px');
+    $("#text").css("left", -(($(window).width() / 2) - (($(".container").width() / 2))) + 'px');
+    $("#text").css("flex-basis", ((($(".container").width() / 2))) + 'px');
+});
+
+//동영상
+$("#cover i").click(function(){
+    $("#cover").html("<iframe width='740' height='400' src='https://www.youtube.com/embed/k7NRCUU5qqQ?autoplay=1&mute=1'; allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+})
+
+
+    $("#cover div").hover(function(){
+        $(this).css("animation-play-state","paused")
+    },function(){$(this).css("animation-play-state","running")
+})
+
+//숫자증가
+
+    var count1=count2=count3=count4=0;
+    counter();
+    function counter(){
+        time1=setInterval(count1Fn, 3);
+        function count1Fn(){
+            count1++;
+            if(count1>532){
+                clearInterval(time1);
+            }else{
+                $(".countNum").eq(0).text(count1);
+            }
+        }
+        time2=setInterval(count2Fn, 5);
+        function count2Fn(){
+            count2++;
+            if(count2>312){
+                clearInterval(time2);
+            }else{
+                $(".countNum").eq(1).text(count2);
+            }
+        }
+        time3=setInterval(count3Fn, 7);
+        function count3Fn(){
+            count3++;
+            if(count3>220){
+                clearInterval(time3);
+            }else{
+                $(".countNum").eq(2).text(count3);
+            }
+        }
+        time4=setInterval(count4Fn, 30);
+        function count4Fn(){
+            count4++;
+            if(count4>32){
+                clearInterval(time4);
+            }else{
+                $(".countNum").eq(3).text(count4);
+            }
+        }
+    }
+
